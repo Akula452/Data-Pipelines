b3a369ef3ee92d0f314b9cdada6d1995bf04fbdf
Prasad Akula <vprasad.java@gmail.com>
2026-02-07 20:37:36 -0500

    added Cli code

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import chalk from "chalk";
+
+export function run(args) {
+  const command = args[2];
+
+  switch (command) {
+    case "hello":
+      console.log(chalk.green("👋 Hello from your custom CLI!"));
+      break;
+
+    case "version":
+      console.log("mycli version 1.0.0");
+      break;
+
+    default:
+      console.log(`
+${chalk.blue("mycli")} - Custom CLI Tool
+
+Usage:
+  mycli hello        Prints greeting
+  mycli version      Prints version
+`);
+  }
+}
